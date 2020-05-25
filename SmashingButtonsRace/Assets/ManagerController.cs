03ec975b043fb6c75024104956445f874d534453
Fernando Guillen <fguillen.mail@gmail.com>
2020-05-25 11:57:03 +0200

    Manager to controll all elements

---
+++ SmashingButtonsRace/Assets/ManagerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManagerController : MonoBehaviour
+{
+  public GameObject ballFloating;
+  public GameObject ballFloatingDirection;
+  public GameObject ballFloatingScale;
+  public Text ballFloatingScaleText;
+  public float ballFloatingAppliedForce = 1f;
+
+  public GameObject ballRunning;
+  public GameObject ballRunningVelocity;
+  public Text ballRunninngVelocity;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+        
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if(Input.GetKeyDown(KeyCode.Space))
+    {
+      ballFloating.GetComponent<Rigidbody>().AddForce(new Vector3(0f, ballFloatingAppliedForce, 0f));
+    }
+  }
+}
