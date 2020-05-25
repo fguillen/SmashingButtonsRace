563540cc16a37cb61b05bfea4ac1e17a307fa7f0
Fernando Guillen <fguillen.mail@gmail.com>
2020-05-25 15:19:50 +0200

    Using Spring formula

--- SmashingButtonsRace/Assets/ManagerController.cs
+++ SmashingButtonsRace/Assets/ManagerController.cs
@@ -7,17 +7,19 @@ public class ManagerController : MonoBehaviour
 {
   public static ManagerController instance;
 
+  // Ball floating
   public GameObject ballFloating;
   public Transform ballFloatingContactPoint;
   public Transform boxContactPoint;
-  public GameObject ballFloatingDirectionArrow;
-  public GameObject ballFloatingDistanceArrow;
-  public float ballFloatingDistance;
+  public float ballFloatingSpringConstant;
+  private float ballFloatingSpringForce;
+  private float ballFloatingDistance;
+  public Text ballFloatingSpringForceText;
   public Text ballFloatingDistanceText;
-  public float ballFloatingAppliedForce = 1f;
+  public float ballFloatingAppliedForce;
 
+  // Ball running
   public GameObject ballRunning;
-  public GameObject ballRunningVelocityArrow;
   public Text ballRunningVelocityText;
   public float ballRunningVelocity;
   public float ballRunningActualVelocity;
@@ -36,18 +38,24 @@ public class ManagerController : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
-    if(Input.GetKeyDown(KeyCode.Space))
-    {
-      ballFloating.GetComponent<Rigidbody2D>().AddForce(new Vector3(0f, ballFloatingAppliedForce, 0f));
-    }
 
+    AddFloatingForce();
     CalculateBallFloatingDistance();
     CalculateBallRunningVelocity();
+    CalculateFloatingBallSpringForce();
   }
 
   public void EndRace()
   {
     Debug.Log("Race ends");
+  }
+
+  void AddFloatingForce()
+  {
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+      ballFloating.GetComponent<Rigidbody2D>().AddForce(new Vector3(0f, ballFloatingAppliedForce, 0f));
+    }
   }
 
   void CalculateBallFloatingDistance()
@@ -62,5 +70,15 @@ public class ManagerController : MonoBehaviour
     ballRunning.GetComponent<Rigidbody2D>().velocity = new Vector3(ballRunningActualVelocity, 0f, 0f);
 
     ballRunningVelocityText.text = ballRunningActualVelocity.ToString();
+  }
+
+  // F=kx
+  // - https://en.wikipedia.org/wiki/Hooke's_law
+  void CalculateFloatingBallSpringForce()
+  {
+    ballFloatingSpringForce = ballFloatingSpringConstant * ballFloatingDistance;
+    ballFloating.GetComponent<Rigidbody2D>().AddForce(new Vector3(0f, ballFloatingSpringForce * -1, 0f));
+
+    ballFloatingSpringForceText.text = ballFloatingSpringForce.ToString();
   }
 }
